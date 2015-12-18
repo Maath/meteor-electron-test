991473e8f126391e904b708b88f649ad07c6b1f9
Michael Risse <Michael Risse>
2015-12-18 11:56:46 -0800

    a Gruntfile working with Windows

--- .test/Gruntfile.js
+++ .test/Gruntfile.js
@@ -2,12 +2,14 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     'create-windows-installer': {
-      x64: {
-        appDirectory: '/Users/mike/electron-test/.meteor-electron/win32-x64/builds/Electron-win32-x64',
-        outputDirectory: '/tmp/build/installer64',
-        authors: 'MixMax-for-Windows',
-        exe: 'Electron.exe',
-        remoteReleases: "http://104.131.109.93:8000/installer64/"
+      ia32: {
+        appDirectory: 'C:\\Users\\Michael\\meteor-electron-test\\.meteor-electron\\win32-ia32\\builds\\mixmax-win32-ia32',
+        outputDirectory: 'C:\\Users\\Michael\\Electron-Builds',
+        authors: 'MixMax Team',
+        exe: 'mixmax.exe',
+        productName: "MixMax",
+        certificateFile: "C:\\Users\\Michael\\Desktop\\MixmaxWindowsCodesigningCertificate.p12",
+//        remoteReleases: "http://104.131.109.93:8000/installer64/"
       }
     }
   });
